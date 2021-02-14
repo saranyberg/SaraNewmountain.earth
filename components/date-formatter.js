93beeeb73e558556b087d50a1e257ed53ce1c567
Samuel Plumppu <greenheartmusic@gmail.com>
2021-02-14 17:34:12 +0100

    Remove unnecessary dependency and replace with web standard

--- components/date-formatter.js
+++ components/date-formatter.js
@@ -1,12 +1,14 @@
-import { parseISO, format } from 'date-fns'
+import { LOCALE } from '../lib/constants'
 
 export default function DateFormatter({ dateString }) {
-    const date = parseISO(dateString)
+    const options = { month: 'long', day: 'numeric', year: 'numeric' }
+    const date = new Date(dateString)
+    const americanDate = new Intl.DateTimeFormat(LOCALE, options).format(date)
     return (
         <time
             dateTime={dateString}
             className="uppercase tracking-wider text-sm">
-            {format(date, 'LLLL	d, yyyy')}
+            {americanDate}
         </time>
     )
 }

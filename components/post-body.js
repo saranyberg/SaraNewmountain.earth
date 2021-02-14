28232515af09fac2600e9fee808f237bfc3f1803
Samuel Plumppu <greenheartmusic@gmail.com>
2021-02-14 17:19:55 +0100

    Clean up post body

--- components/post-body.js
+++ components/post-body.js
@@ -1,12 +1,8 @@
-import markdownStyles from './markdown-styles.module.css'
-
 export default function PostBody({ content }) {
-  return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
-  )
+    return (
+        <article
+            className="max-w-2xl mx-auto"
+            dangerouslySetInnerHTML={{ __html: content }}
+        />
+    )
 }

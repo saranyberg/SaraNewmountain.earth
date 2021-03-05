2d258ea4a351f1c330433ef1d26d9ee67c846a73
Samuel Plumppu <greenheartmusic@gmail.com>
2021-03-05 22:16:59 +0100

    add todos and CTA

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 import Container from './container'
-import { BLOG_TAGLINE, EXAMPLE_PATH } from '../lib/constants'
+import { EXAMPLE_PATH } from '../lib/constants'
 
 export default function Footer() {
     return (
@@ -7,19 +7,32 @@ export default function Footer() {
             <Container>
                 <div className="py-28 flex flex-col lg:flex-row items-center">
                     <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-                        {BLOG_TAGLINE}
+                        Let's take action together.
+                        <br />
+                        What's your next move?
                     </h3>
                     <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
                         <a
                             href="https://nextjs.org/docs/basic-features/pages"
                             className="mx-4 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
-                            Read Documentation
+                            I want to take action
                         </a>
                         <a
                             href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
                             className="mx-4 font-bold hover:underline">
-                            View on GitHub
+                            I want to learn more
                         </a>
+                        {/* TODO:
+                        
+                        - add page for "take action"
+                        - add page for "learn more"
+                        - add intro about me
+                        tudelinoodlez@tuta.io
+
+                        - remove post author
+                        - move up date above main post image
+
+                        */}
                     </div>
                 </div>
             </Container>

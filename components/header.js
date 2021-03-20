14e28c6c6112d9ccc2c73ba9f115815e0d948ad7
Samuel Plumppu <greenheartmusic@gmail.com>
2021-03-20 18:20:10 +0100

    Improve menu alignment

--- components/header.js
+++ components/header.js
@@ -11,21 +11,21 @@ const backgrounds = {
 export default function Header({ bg = 'green' }) {
     const background = backgrounds[bg]
     return (
-        <header
-            className={`w-full mb-8 md:mb-12 p-4 ${background} text-center flex items-center justify-between shadow-lg`}
-        >
-            <span className="text-5xl -mt-4">🏔</span>
-            <div>
-                <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight">
-                    <Link href="/">
-                        <a className="hover:underline">{BLOG_NAME}</a>
-                    </Link>
-                </h2>
-                <p className="mt-2 text-sm md:text-md lg:text-lg italic">
-                    {BLOG_TAGLINE}
-                </p>
+        <header className={`w-full mb-8 md:mb-12 ${background} shadow-lg`}>
+            <div className="container text-center flex items-center justify-between mx-auto p-4">
+                <span className="text-5xl -mt-4">🏔</span>
+                <div>
+                    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight">
+                        <Link href="/">
+                            <a className="hover:underline">{BLOG_NAME}</a>
+                        </Link>
+                    </h2>
+                    <p className="mt-2 text-sm md:text-md lg:text-lg italic">
+                        {BLOG_TAGLINE}
+                    </p>
+                </div>
+                <MobileMenu />
             </div>
-            <MobileMenu />
         </header>
     )
 }

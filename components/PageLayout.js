daa29d6b1587e8f4119e3e7e5d874cf245b0747a
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-01-13 20:30:05 +0100

    Updates

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -6,7 +6,7 @@ export default function PageLayout({ children }) {
     return (
         <Layout>
             <Header />
-            <Container className="max-w-max bg-white bg-opacity-80 mx-auto rounded-md p-4 sm:p-8 md:p-16 mb-32">
+            <Container className="max-w-max bg-white bg-opacity-90 mx-auto rounded-md p-4 sm:p-8 md:p-16 mb-32">
                 <article className="prose lg:prose-lg 2xl:prose-xl">
                     {children}
                 </article>

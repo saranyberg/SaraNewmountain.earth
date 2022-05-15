afbfe57fc63da8ac9e0b65d72c6bab8a2f3d63d2
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-05-15 20:16:39 +0200

    Update date formatter

--- components/DateFormatter.js
+++ components/DateFormatter.js
@@ -1,17 +1,15 @@
-import { LOCALE } from '../lib/constants'
-import { cx } from '../lib/utils'
+import { cx, formatDate } from '../lib/utils'
 
 const DateFormatter = ({ dateString, className }) => {
-    const options = { month: 'long', day: 'numeric', year: 'numeric' }
-    const formatted = new Intl.DateTimeFormat(LOCALE, options).format(
-        new Date(dateString),
-    )
     return (
         <time
             dateTime={dateString}
-            className={cx('uppercase tracking-wider text-sm block', className)}
+            className={cx(
+                'uppercase tracking-wider text-base block',
+                className,
+            )}
         >
-            {formatted}
+            {formatDate(dateString)}
         </time>
     )
 }
